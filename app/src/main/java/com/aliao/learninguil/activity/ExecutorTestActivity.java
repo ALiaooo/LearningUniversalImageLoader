58580aad9f930db682d279e933d01849b187c365
阿廖 <liaolishuang@gmail.com>
2015-06-25 10:29:15 +0800

    Initial commit

---
+++ app/src/main/java/com/aliao/learninguil/activity/ExecutorTestActivity.java
@@ -0,0 +1,18 @@
+package com.aliao.learninguil.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aliao.learninguil.R;
+
+/**
+ * Created by ÀöË« on 2015/6/25.
+ */
+public class ExecutorTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_executor);
+    }
+}
